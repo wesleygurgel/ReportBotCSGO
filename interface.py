9119dbe1d63b8e588a92b5798344c32dc3111ad9
Wesley <wesleygurgel27@gmail.com>
2021-03-04 12:28:44 -0300

    STEAMPLAYER INPUT

--- interface.py
+++ interface.py
@@ -9,47 +9,45 @@ class ReportBot:
 
     def __init__(self):
         self.root = tk.Tk()
-        self.report1_entry = None
-        self.report2_entry = None
-        self.report3_entry = None
+        self.frame_id_jogadores = tk.LabelFrame(self.root, text="Elementos para Reportar", padx=40, pady=15)
+        self.report1_entry = tk.Entry(self.frame_id_jogadores, width=70)
+        self.report2_entry = tk.Entry(self.frame_id_jogadores, width=70)
+        self.report3_entry = tk.Entry(self.frame_id_jogadores, width=70)
         self.driver = None
         self.community = tk.StringVar()
         self.a, self.b, self.c, self.d, self.e = tk.BooleanVar(), tk.BooleanVar(), tk.BooleanVar(), \
                                                  tk.BooleanVar(), tk.BooleanVar()
+        self.jogadores = []
 
     def criar_interface(self):
         self.root.title("Report CSGO Players")
 
-        frame_id_jogadores = tk.LabelFrame(self.root, text="Elementos para Reportar", padx=40, pady=15)
-        frame_id_jogadores.grid(row=0, column=0, sticky=tk.E + tk.N + tk.S + tk.W, ipadx=5, ipady=5, padx=5, pady=5)
+        self.frame_id_jogadores.grid(row=0, column=0, sticky=tk.E + tk.N + tk.S + tk.W, ipadx=5, ipady=5, padx=5, pady=5)
 
-        label_explicacao = tk.Label(frame_id_jogadores, text="Abaixo coloque os perfis que deseja denunciar:")
+        label_explicacao = tk.Label(self.frame_id_jogadores, text="Abaixo coloque os perfis que deseja denunciar:")
         label_explicacao.config(font=(20))
         label_explicacao.grid(row=0, column=0, columnspan=2, pady=20, padx=20)
 
         # REPORTSSSSS PLAYERS
-        frame_id_jogadores.columnconfigure(0, weight=1)
-        frame_id_jogadores.columnconfigure(1, weight=2)
+        self.frame_id_jogadores.columnconfigure(0, weight=1)
+        self.frame_id_jogadores.columnconfigure(1, weight=2)
 
         # PERFIL 1
-        report1_label = tk.Label(frame_id_jogadores, text="Perfil 1:")
+        report1_label = tk.Label(self.frame_id_jogadores, text="Perfil 1:")
         report1_label.grid(row=1, column=0, sticky=tk.E, padx=20)
 
-        self.report1_entry = tk.Entry(frame_id_jogadores, width=70)
         self.report1_entry.grid(row=1, column=1, padx=20, pady=20, sticky=tk.W + tk.E)
 
         # PERFIL 2
-        report2_label = tk.Label(frame_id_jogadores, text="Perfil 2:")
+        report2_label = tk.Label(self.frame_id_jogadores, text="Perfil 2:")
         report2_label.grid(row=2, column=0, sticky=tk.E, padx=20, pady=(0, 20))
 
-        self.report2_entry = tk.Entry(frame_id_jogadores, width=70)
         self.report2_entry.grid(row=2, column=1, padx=20, pady=(0, 20), sticky=tk.W + tk.E)
 
         # PERFIL 3
-        report3_label = tk.Label(frame_id_jogadores, text="Perfil 3:")
+        report3_label = tk.Label(self.frame_id_jogadores, text="Perfil 3:")
         report3_label.grid(row=3, column=0, sticky=tk.E, padx=20, pady=(0, 20))
 
-        self.report3_entry = tk.Entry(frame_id_jogadores, width=70)
         self.report3_entry.grid(row=3, column=1, padx=20, pady=(0, 20), sticky=tk.W + tk.E)
 
         # FRAME TIPOS DE REPORT ----------------------------------------------------------------------------------------
@@ -104,14 +102,21 @@ class ReportBot:
         self.root.mainloop()
 
     def iniciar_selenium(self):
-        # self.driver = webdriver.Chrome(executable_path='chromedriver.exe')
-        # self.driver.get('https://extremereportbot.com/home/')
-        # print(self.community.get())
-        # print(self.a.get())
-        # print(self.b.get())
-        # time.sleep(1)
-        pass
+        self.driver = webdriver.Chrome(executable_path='chromedriver.exe')
+        self.driver.get('https://extremereportbot.com/home/')
+        self.pegar_jogadores()
 
+        continuar = True
+        while continuar:
+            for steamplayer in self.jogadores:
+                self.driver.find_element(By.ID, 'steamuser').send_keys(steamplayer)
+                # print(self.community.get())
+                # print(self.a.get())
+                # print(self.b.get())
+                time.sleep(10)
+
+    def pegar_jogadores(self):
+        self.jogadores.extend((self.report1_entry.get(), self.report2_entry.get(), self.report3_entry.get()))
 
 meubot = ReportBot()
 meubot.criar_interface()
